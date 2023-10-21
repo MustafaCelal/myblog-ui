e34505a70b0fb36055328a2a85e61d23648b0bb4
MustafaCelal <m.cllglr@gmail.com>
2023-10-21 14:01:59 +0300

    Main Screen (tabBar) modified

--- classic/src/view/main/Main.js
+++ classic/src/view/main/Main.js
@@ -1,10 +1,3 @@
-/**
- * This class is the main view for the application. It is specified in app.js as the
- * "mainView" property. That setting automatically applies the "viewport"
- * plugin causing this view to become the body element (i.e., the viewport).
- *
- * TODO - Replace this content of this view to suite the needs of your application.
- */
 Ext.define('MySpace.view.main.Main', {
     extend: 'Ext.tab.Panel',
     xtype: 'app-main',
@@ -22,54 +15,9 @@ Ext.define('MySpace.view.main.Main', {
 
     ui: 'navigation',
 
-    tabBarHeaderPosition: 1,
-    titleRotation: 0,
-    tabRotation: 0,
-
-    header: {
-        layout: {
-            align: 'stretchmax'
-        },
-        title: {
-            bind: {
-                text: '{name}'
-            },
-            flex: 0
-        },
-        iconCls: 'fa-th-list'
-    },
-
     tabBar: {
-        flex: 1,
         layout: {
-            align: 'stretch',
-            overflowHandler: 'none'
-        }
-    },
-
-    responsiveConfig: {
-        tall: {
-            headerPosition: 'top'
-        },
-        wide: {
-            headerPosition: 'left'
-        }
-    },
-
-    defaults: {
-        bodyPadding: 20,
-        tabConfig: {
-            responsiveConfig: {
-                wide: {
-                    iconAlign: 'left',
-                    textAlign: 'left'
-                },
-                tall: {
-                    iconAlign: 'top',
-                    textAlign: 'center',
-                    width: 120
-                }
-            }
+            pack: 'center'
         }
     },
 
@@ -87,7 +35,7 @@ Ext.define('MySpace.view.main.Main', {
         }
     }, {
         title: 'NOTES',
-        iconCls: 'fa-window-restore',
+        iconCls: 'fa-window-restore sidebar-tab-icon',
         bind: {
             html: '{loremIpsum}'
         }
